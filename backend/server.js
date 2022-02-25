e70740697c203a38ed8652fb33bb08033e2541f7
AviFS <avifsternlieb@gmail.com>
2022-02-25 02:11:05 -0800

    fixed with async sleep

--- backend/server.js
+++ backend/server.js
@@ -8,23 +8,50 @@ let delimiter_out = "out: 666666"
 
 let response = {out: "", err: ""}
 
+let globals = {};
+
 front.on('connection', function connection(ws) {
+  globals.ws = ws;
   ws.send('Connected');
 
-  ws.on('message', function message(msg) {
+  ws.on('message', async function message(msg) {
+    // added the aync sleep because i was still every now and then getting the err in the next one
+    // was something getting this:
+    // ?
+    // {"out":"","err":""}
+    // 2
+    // {"out":"2\n","err":"(standard_in) 41: illegal character: ?\n"}
+
+    // instead of always getting this:
+    // ?
+    // {"out":"","err":"(standard_in) 41: illegal character: ?\n"}
+    // 2
+    // {"out":"2\n","err":"""}
+
     // ws.send('!'+data);
     run.send(msg);
+    await new Promise(resolve => setTimeout(resolve, 1));
     run.send(delimiter_in)
   });
+});
+
+front.on('close', function close() {
+    console.log('disconnected');
+});
 
 run.on('message', function message(data) {
     let event = {"data": String(data)};
         if (event.data == delimiter_out) {
-            console.log(response)
-        ws.send(JSON.stringify(response));
+            // console.log('---')
+            // console.log(response)
+        globals.ws.send(JSON.stringify(response));
+
         response = {out: "", err: ""};
         return;
     } 
+    else {
+        // console.log(event.data);
+    }
     // else {console.log(1)}
 
     switch (event.data.substring(0,5)) {
@@ -37,7 +64,6 @@ run.on('message', function message(data) {
         default:
             outputPrefaceError();
     }
-});
 });
 
 // run.on('message', function message(msg) {

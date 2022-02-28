24137957bc2898d952823ff9a7601d8e83df5916
AviFS <avifsternlieb@gmail.com>
2022-02-28 14:51:19 -0800

    stuff

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-let ws = new WebSocket('ws://127.0.0.1:8000/');
-ws.send
+let ws;
+
+function setLang() {
+    let lang = document.getElementById('lang').value;
+    console.log(lang)
+    // lang = 'cat';
+    ws = new WebSocket('ws://54.153.39.161:8000/'+lang);
+    // ws = new WebSocket('ws://127.0.0.1:8001/'+lang);
+    // ws = new WebSocket('ws://localhost:8000/cat');
+
+    ws.onmessage = function(event) {
+        console.log(event.data)
+        document.getElementById('result').innerText = event.data;
+    }
+}
+
 
 function run() {
     let text = document.getElementById('text').value;
     ws.send(text);
-}
-
-ws.onmessage = function onmessage(event) {
-    document.getElementById('result').innerText = event.data;
 }

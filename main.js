93788b7ffd8a4727daa86471216696826e2738d4
AviFS <avifsternlieb@gmail.com>
2022-03-01 19:04:49 -0800

    moved funcs.js to main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 let ws;
 
+function updateLine(line, data) {
+    // console.log(line, data)
+    document.getElementById('right').children[line].innerHTML = data;
+}
+function getLineNumber() {
+    let textarea = document.getElementById('left');
+    return textarea.value.substr(0, textarea.selectionStart).split("\n").length-1;
+}
+
+function linesUpdate() {
+    lines = document.getElementById('left').value.split("\n");
+}
+function getLine(lineNum) {
+    // this is also a dumb function right now, but might be dynamically smart later
+    return lineNum < lines.length? lines[lineNum]: "";
+}
+
 function init() {
     lines = [];
     // ws = new WebSocket('ws://54.153.39.161:8003/');
@@ -68,7 +85,7 @@ function simple_onmessage(event) {
     }
     else {
         // updateLine(data.line, "*")
-        timer = setTimeout(errorCallback, 1500, data)
+        timer = setTimeout(errorCallback, 1000, data)
     }
 }
 

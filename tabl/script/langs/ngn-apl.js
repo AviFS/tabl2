07586753b6df1f173cfda2e41483092ef12765aa
AviFS <avifsternlieb@gmail.com>
2022-03-19 00:33:19 -0700

    ngn-apl: needs polishing but right side is now being updated properly with divs

--- tabl/script/langs/ngn-apl.js
+++ tabl/script/langs/ngn-apl.js
@@ -10,18 +10,25 @@ class ngnAPL extends Lang {
         
         function format(expr) { return apl.fmt(expr).join('\n'); }
 
-        let display = ""
-        for (let i=0; i<lines.length; i++) {
-            if (!lang.isIgnore(lines[i])) {
+        // if we're on a new line, add a new div
+        if (document.getElementById('right').children.length < lines.length) {
+            let missing = lines.length - document.getElementById('right').children.length;
+            document.getElementById('right').innerHTML += "<div class='row'></div>".repeat(missing);
+        }
+
+        let right = document.getElementById('right').children;
+
+        for (let i=0; i<right.length; i++) {
+            if (i<lines.length && !lang.isIgnore(lines[i])) {
                 let prog = lines.slice(0, i+1).join('\n')
-                display += format(apl(prog))+'\n';
+                let result = format(apl(prog));
+                updateLine(i, result);
             }
             else {
-                display += "\n";
+                updateLine(i, "");
             }
         }
 
-        document.getElementById('right').innerHTML = display;
         // document.getElementById('output').innerHTML = ...;
 
     }

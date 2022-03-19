ac330e544d50a3b85bcb7de88f9fcec22c01c723
AviFS <avifsternlieb@gmail.com>
2022-03-18 17:39:15 -0700

    pip: turns out Exception and BaseException are not the same. BaseException catches SystemExits and pip is now officially stable and getting merged and deployed to the server!

--- tabl/bin/pip/pip/repl.py
+++ tabl/bin/pip/pip/repl.py
@@ -1,3 +1,4 @@
+from lib2to3.pytree import Base
 import sys
 import json
 import pip
@@ -19,8 +20,15 @@ def print(msg, end="\n", file=sys.stdout):
 def run(code, argv=[5,8,2]):
     global stdout, stderr
     stdout, stderr = "", ""
-    pip.pip(code, argv)
-    result = {"disp": stdout.strip(), "console": {"error": stderr}}
+    try:
+        pip.pip(code, argv)
+    except BaseException as e:
+        isError = True
+        __print("error", file=sys.stderr)
+    else:
+        isError = False
+
+    result = {"disp": stdout.strip(), "console": {"error": stderr}, "isError": isError}
     return result
 
 # pip.__builtins__["input"] = input
@@ -41,17 +49,13 @@ def repl(disable_json=False):
             break
         if not disable_json:
             inputJSON = json.loads(inp)
+            if inputJSON['input'] == "":
+                inputJSON['input'] = [5,8,2]
         else:
             inputJSON = {"line": 0, "code": inp, "input": [5,8,2]}
 
-        try:
-            outputJSON = run(inputJSON['code'], inputJSON['input'])
-        except Exception as e:
-            outputJSON['err'] += e
-            outputJSON['isError'] = True
-        else:
-            outputJSON['isError'] = False
-        
+        outputJSON = run(inputJSON['code'], inputJSON['input'])
+
         outputJSON['line'] = inputJSON['line']
             
         __print(json.dumps(outputJSON))

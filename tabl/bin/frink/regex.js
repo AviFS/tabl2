f05d0063700d61335d4843aaf8cd0afb5d6299ff
AviFS <avifsternlieb@gmail.com>
2022-03-19 23:16:21 -0700

    added float/int/frac to regex

--- tabl/bin/frink/regex.js
+++ tabl/bin/frink/regex.js
@@ -1,16 +1,28 @@
-num = "(.)?"
+let float = String.raw `(\d+(?:\.\d+)?)`; // float or int
 
-expr = num + "e" + num
+let int = String.raw `(\d+)`;
+let frac = String.raw `${int}\/${int}`
+
+let expr = `(?:${float}|${frac})`;
+
+console.log(expr);
 const re = new RegExp("^"+expr+"$")
 
 let inps = `
-def
-ep
-ke
-nomatch
+
+1/4
+23
+23/695
+0.234
+
 `.trim()
 
 for (const inp of inps.split('\n')) {
     let matches = inp.match(re)
-    console.log(matches)
+    if (matches == null) {
+        console.log("null")
+    }
+    else {
+        console.log(`${matches[0]}: ${matches.slice(1).join(' ')}`);
+    }
 }

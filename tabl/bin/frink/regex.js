c7bfc7feca6c20d3117b8374a619a2d7d614c87d
AviFS <avifsternlieb@gmail.com>
2022-03-20 00:18:49 -0700

    frink-regex: this is catching the unit series beautifully, but having m^0 and m^1 are both caught the same (as undefined) because it's only looking at the exponent, and unfortunately there doesn't seem to be a way to set a default value for a capture group. setting the default value to 1 would solve everything. real bummer!

--- tabl/bin/frink/regex.js
+++ tabl/bin/frink/regex.js
@@ -5,19 +5,28 @@ let frac = String.raw `${int}\/${int}`
 
 let imaginary = String.raw ` (i)?`
 
+// i'm realizing that since you can change the base/display units, i should really just generalize this
+// to match any [text]^[int], and do a repeated thing of that capturing both the unit and exp for each
+
 let approx = String.raw `(exactly|approx\.) ${float}`;
-let unit = (base) => String.raw `(?: ${base}(?:\^${int})?)?`; // leading space required for match
+let unit = (base) => String.raw `(?: ${base}(?:\^${int})?)?`; // note the leading space is already included
+// list from: https://frinklang.org/#HowFrinkIsDifferent
+// i then ran it in the frink repl to see how it outputs and in what fixed order the units get displayed:
+//     m kg s A cd mol K bit USD
+//     1 m s kg A K dollar mol bit cd (unknown unit type)
+let baseUnits = ["m", "s", "kg", "A", "K", "dollar", "mol", "bit", "cd"].slice(0,3)
 
 // let p1 = String.raw `(?:${float}|${frac})`;
 let p1_a = String.raw `${float}`
 let p1_b = String.raw `${frac} \(${approx}\)`
 let p1 = String.raw `(?:${p1_a}|${p1_b})`
 
-let p2 = String.raw `23`
+let p2 = baseUnits.map(unit).join(""); // again, note these are joined with the empty string, not a space
 
-let expr = `1${unit("kg")}`;
-expr = "^"+expr+"$";
-console.log(expr);
+
+let expr = p2;
+expr = "^"+float+expr+"$";
+// console.log(expr);
 const re = new RegExp(expr)
 
 // let inps = `
@@ -28,10 +37,10 @@ const re = new RegExp(expr)
 // `.trim()
 
 let inps = `
-1 kg
-1 kg^2
-1 kg^29304
-1 kg^-3
+0.5 m s kg
+3 s^2
+4 m s^-1 kg
+2.9 m^3 kg^2
 `.trim()
 
 for (const inp of inps.split('\n')) {
@@ -40,7 +49,8 @@ for (const inp of inps.split('\n')) {
         console.log("null")
     }
     else {
-        console.log(`${matches[0]}: ${matches.slice(1).join(' ')}`);
+        console.log(matches.slice(1).map(x => x==undefined? '_': x).map(x => x.padStart(6, ' ')).join(' '));
+        // console.log(`${(matches[0]+':').padEnd(20, ' ')} ${matches.slice(1).map(x => x==undefined? '_': x).map(x => x.padStart(6, ' ')).join(' ')}`);
         // console.log(matches)
     }
 }

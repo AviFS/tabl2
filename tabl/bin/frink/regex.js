6ccdbf67d1fc728bab7da2efda37da9b08749d4a
AviFS <avifsternlieb@gmail.com>
2022-03-19 23:31:06 -0700

    frink-regex: p1 is done, just need the unit bits now

--- tabl/bin/frink/regex.js
+++ tabl/bin/frink/regex.js
@@ -3,18 +3,22 @@ let float = String.raw `(\d+(?:\.\d+)?)`; // float or int
 let int = String.raw `(\d+)`;
 let frac = String.raw `${int}\/${int}`
 
-let expr = `(?:${float}|${frac})`;
+let approx = String.raw `(exactly|approx\.) ${float}`;
+// let p1 = String.raw `(?:${float}|${frac})`;
+let p1_a = String.raw `${float}`
+let p1_b = String.raw `${frac} \(${approx}\)`
+let p1 = String.raw `(?:${p1_a}|${p1_b})`
 
+let expr = p1;
+expr = "^"+expr+"$";
 console.log(expr);
-const re = new RegExp("^"+expr+"$")
+const re = new RegExp(expr)
 
 let inps = `
-
-1/4
-23
-23/695
-0.234
-
+1/4 (exactly 0.25)
+0.8
+2/3 (approx. 0.666666)
+2045
 `.trim()
 
 for (const inp of inps.split('\n')) {
@@ -24,5 +28,6 @@ for (const inp of inps.split('\n')) {
     }
     else {
         console.log(`${matches[0]}: ${matches.slice(1).join(' ')}`);
+        // console.log(matches)
     }
 }

e6631e7bb54f3f8d4e23ae9764977cc1dd851e62
AviFS <avifsternlieb@gmail.com>
2022-03-21 00:50:09 -0700

    frink-display: committing now because these changes are for keeps and i'm about to make a temp fix that i *won't* want to keep

--- tabl/script/langs/frink.js
+++ tabl/script/langs/frink.js
@@ -11,9 +11,12 @@ class Frink extends Lang {
     static init() {
         [
             // "showApproximations[false]",
-            "rationalAsFloat[true]",
-            "setPrecision[10]",
-            "showDimensionName[false]",
+
+            // these three were my defaults before parsing frink's output on frontend
+            // "rationalAsFloat[true]",
+            // "setPrecision[10]",
+            // "showDimensionName[false]",
+
         ].forEach(code => send(ws, {"code": code}));
         // More efficient if .join(";") and send it in one JSON
     }
